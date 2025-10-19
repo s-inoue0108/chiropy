dff7a8ad5734771c300fb8ef7de4e568fc75e553
s-inoue-ku <inoueshota@SMI20245.local>
2025-10-20 06:05:21 +0900

    add molecular file format: cif

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="chiropy",
-    version="1.0.0",
+    version="1.0.1",
     description="CHIROpy is a Gaussian binding tool for analyze chiroptical properties.",
     long_description=long_description,
     long_description_content_type="text/markdown",
